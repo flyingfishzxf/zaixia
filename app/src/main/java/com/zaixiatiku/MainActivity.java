e6778b382ad743a85c0c42c9192e1e4d84bb05ae
673157487@qq.com <flyingfishzxf@gmail.com>
2018-06-04 16:02:34 +0800

    完成登录页布局

--- app/src/main/java/com/zaixiatiku/MainActivity.java
+++ app/src/main/java/com/zaixiatiku/MainActivity.java
@@ -1,13 +1,33 @@
 package com.zaixiatiku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toLogin = findViewById(R.id.to_login);
+        toLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.to_login:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
